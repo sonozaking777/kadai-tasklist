3c761cf7d697376d6a1fefc1d219bd3e14e10618
r.sonozaki <sonozaki7789@yahoo.co.jp>
2017-10-11 13:39:26 +0000

    課題再々提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -64,7 +64,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to signup_path
+      redirect_to root_url
     end
   end
 end
